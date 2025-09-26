79a3166edb60cf69c40bbda30b9b9a252cf869bf
Dedsectao <onotaozebua@gmail.com>
2025-09-26 15:39:02 +0700

    Update portfolio

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const resumeList = document.querySelectorAll(".resume-list");
 const resumeBox = document.querySelectorAll(".resume-box");
 
+const portfolioList = document.querySelectorAll(".portfolio-list");
+const portfolioBox = document.querySelectorAll(".portfolio-box");
 // resume section when clicking tab-list
 resumeList.forEach((list, idx) => {
   list.addEventListener("click", () => {
@@ -9,5 +11,16 @@ resumeList.forEach((list, idx) => {
 
     document.querySelector(".resume-box.active").classList.remove("active");
     resumeBox[idx].classList.add("active");
+  });
+});
+
+// portfolio section when clicking tab-list
+portfolioList.forEach((list, idx) => {
+  list.addEventListener("click", () => {
+    document.querySelector(".portfolio-list.active").classList.remove("active");
+    list.classList.add("active");
+
+    document.querySelector(".portfolio-box.active").classList.remove("active");
+    portfolioBox[idx].classList.add("active");
   });
 });
