8085ffcde22a566b3a1c2b10997e785d55a718a2
Dedsectao <onotaozebua@gmail.com>
2025-09-30 14:13:39 +0700

    updatesection

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+const navs = document.querySelectorAll(".nav-list li");
+const cube = document.querySelector(".box");
+const sections = document.querySelectorAll(".section");
+
 const resumeList = document.querySelectorAll(".resume-list");
 const resumeBox = document.querySelectorAll(".resume-box");
 
 const portfolioList = document.querySelectorAll(".portfolio-list");
 const portfolioBox = document.querySelectorAll(".portfolio-box");
+
+// navbar action and all section along with cube rotation when navbar is clicked
+navs.forEach((nav, idx) => {
+  nav.addEventListener("click", () => {
+    document.querySelector(".nav-list li.active").classList.remove("active");
+    nav.classList.add("active");
+
+    cube.style.transform = `rotateY(${idx * -90}deg)`;
+
+    document.querySelector(".section.active").classList.remove("active");
+    sections[idx].classList.add("active");
+
+    const array = Array.from(sections);
+    const arrSecs = array.slice(1, -1); //only requires indexes 1, 2, 3 or does not require start and End indexes
+    arrSecs.forEach((arrSec) => {
+      if (arrSec.classList.contains("active")) {
+        sections[4].classList.add("action-contact");
+      }
+    });
+    if (sections[0].classList.contains("active")) {
+      sections[4].classList.remove("action-contact");
+    }
+  });
+});
+
 // resume section when clicking tab-list
 resumeList.forEach((list, idx) => {
   list.addEventListener("click", () => {
